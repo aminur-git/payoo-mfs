3d23ead1cf62140d872fc62e9a03d099c14bf28f
aminur-git <aminur.4mi@gmail.com>
2025-02-11 01:34:18 +0600

    another js file with same functionality

---
+++ js/addMoney.js
@@ -0,0 +1,18 @@
+document.getElementById("add-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  const getAmmount = parseFloat(document.getElementById("input-money").value);
+  const getPin = parseFloat(document.getElementById("input-pin").value);
+  if (getPin === 11) {
+    const currentBalance = parseFloat(
+      document.getElementById("current-balance").innerText
+    );
+    const newBalance = currentBalance + getAmmount;
+    document.getElementById("current-balance").innerText = newBalance;
+
+    document.getElementById("input-money").value = "";
+    document.getElementById("input-pin").value = "";
+  } else {
+    alert("Request Dishonored!  Please try again.");
+    document.getElementById("input-pin").value = "";
+  }
+});
