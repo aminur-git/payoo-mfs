75e482d5ffd8ebc699162573c6cde1952b0e8b22
aminur-git <aminur.4mi@gmail.com>
2025-02-11 00:47:57 +0600

    add money feature functioning well

--- styles/js/home.js
+++ styles/js/home.js
@@ -1,11 +1,36 @@
+// step-1 ---> add event listener to 'add button'.
 document.getElementById("add-btn").addEventListener("click", function (event) {
+  // step-2 ---> prevent page from default reloading.
   event.preventDefault();
-  let pinCheck = document.getElementById("input-pin").value;
+  //   step-3 ---> get the pin from input field.
+  const pinCheck = document.getElementById("input-pin").value;
+  //   step-4 ---> get the money to be added.
+  const addMoney = document.getElementById("input-money").value;
+  //   console.log(addMoney)
+
+  // step-5 ---> verify pin number
+  // do if pin is correct:
   if (pinCheck === "5565") {
-    console.log("Amount Added");
+    // get balance:
+    const currentBalance = document.getElementById("current-balance").innerText;
+    console.log(currentBalance);
+
+    // add money with current balance:
+    //                 <------------- Convert string to number ------------>
+    const addMoneyNumber = parseFloat(addMoney);
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    //                 <------------- Add added money to main balance ------------>
+    const newBalance = addMoneyNumber + currentBalanceNumber;
+    // update to main balance display:
+    document.getElementById("current-balance").innerText = newBalance;
+    // clear input field after button click:
     document.getElementById("input-pin").value = "";
-  } else {
-    console.log("Rejected");
+    document.getElementById("input-money").value = "";
+  }
+  //show a error message if pin is invalid
+  else {
+    alert("Failed to add money!   Please try again.");
     document.getElementById("input-pin").value = "";
   }
 });
